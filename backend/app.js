d2ee4234427fcb338845837178e29810302dcaaf
kazukiendo <ka.endo@nagatake.co.jp>
2023-02-09 18:52:33 +0900

    update: connection setting between node and mysql

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,30 @@ const PORT = 3000;
 const cors = require("cors");
 app.use(cors());
 
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "sentence-writer-db",
+  user: "root",
+  password: "mypassword",
+  database: "testdb",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log("error connecting: " + err.stack);
+    return;
+  }
+  console.log("success");
+});
+
+app.get("/connect", (req, res) => {
+  connection.query("SELECT * FROM ", (error, results) => {
+    console.log(results);
+    res.send("successfully connected");
+  });
+});
+
 app.get("/", (req, res) => {
   res.send({ result: "this message has sent from backend!" });
   // fs.readFile("src/index.html", function (err, html) {
