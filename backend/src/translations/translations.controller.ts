321720ad543157c1c4d80f7daa52ae7ec84be9e6
kazukiendo <ka.endo@nagatake.co.jp>
2023-04-01 08:31:06 +0900

    update: moved translate logic into Nest

--- backend/src/translations/translations.controller.ts
+++ backend/src/translations/translations.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import axios from 'axios';
 
-@Controller('translations')
-export class TranslationsController {}
+@Controller()
+export class TranslationsController {
+  // TODO: fix argument type after defining specification.
+  @Post('translation')
+  async translate(@Body() data: any): Promise<string> {
+    let result = '';
+
+    if (data.text && data.target_lang) {
+      const config = {
+        headers: {
+          Authorization: process.env.DEEPL_API_KEY,
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      };
+      const params = {
+        text: data.text,
+        target_lang: data.target_lang,
+      };
+
+      console.log('params', params);
+      await axios
+        .post('https://api-free.deepl.com/v2/translate', params, config)
+        .then((res) => {
+          result = res.data;
+        })
+        .catch((err) => {
+          console.log('error occurred when translating: ', err);
+        });
+    }
+    return result;
+  }
+}
