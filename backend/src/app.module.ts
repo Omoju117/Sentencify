ea893adc60aeafd10ac8ee2cdacebb921c1a1b9e
kazukiendo <ka.endo@nagatake.co.jp>
2023-04-09 10:05:46 +0900

    add: openAI module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { DocumentsModule } from './document/documents.module';
 import { TranslationsModule } from './translations/translations.module';
 import { UserModule } from './user/user.module';
+import { OpenAiModule } from './open-ai/open-ai.module';
 
 @Module({
-  imports: [DocumentsModule, TranslationsModule, UserModule],
+  imports: [DocumentsModule, TranslationsModule, UserModule, OpenAiModule],
   controllers: [AppController],
   providers: [AppService],
 })
